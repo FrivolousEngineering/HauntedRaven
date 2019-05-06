3e496df58c7d711a21c02bbd63a6f0122341e385
Jaime van Kessel <nallath@gmail.com>
2019-05-06 22:18:45 +0200

    Add remote control for the animations

--- Raven.py
+++ Raven.py
@@ -1,5 +1,39 @@
+from serial import SerialException
+
+import maestro
 
 
 class Raven:
     def __init__(self):
-        pass
+        print("Attempting to create serial")
+
+        self.controller = None
+        self.findController()
+
+    def findController(self):
+        for i in range(0, 10):
+            try:
+                port = "/dev/ttyACM%s" % i
+                self.controller = maestro.Controller(ttyStr= port)
+                print("Connected with serial %s" % port)
+                break
+            except:
+                pass
+
+    def nodYes(self):
+        self._checkController()
+        if self.controller:
+            self.controller.runScriptSub(2)
+
+    def nodNo(self):
+        self._checkController()
+        if self.controller:
+            self.controller.runScriptSub(1)
+
+    def _checkController(self):
+        try:
+            self.controller.getPosition(0)
+        except (SerialException, AttributeError):
+            # We failed to write, so it must've gotten disconnected or we were never connected in the first place.
+            self.controller = None
+            self.findController()
--- Server.py
+++ Server.py
@@ -11,10 +11,13 @@ class Server(flask.Flask):
     def __init__(self, import_name="Raven", port=80, **kwargs):
         super().__init__(import_name, **kwargs)
         self._port = port
-        self._radio_instance = Raven()
+        self._raven = Raven()
 
         self.add_url_rule(rule="/<path:path>", view_func=self.staticHost)
         self.add_url_rule("/", endpoint="adminPage", view_func=self.renderAdminPage)
+
+        self.add_url_rule("/startAnimation", endpoint="startAnimation",
+                          view_func=self.startAnimation, methods=["POST"])
 
     def staticHost(self, path):
         return flask.send_from_directory(self.STATIC_LOCATION, path)
@@ -25,6 +28,14 @@ class Server(flask.Flask):
 
     def renderAdminPage(self):
         return render_template("adminPage.html")
+
+    def startAnimation(self):
+        animation_to_start = request.form["animation"]
+        if animation_to_start == "yes":
+            self._raven.nodYes()
+        elif animation_to_start == "no":
+            self._raven.nodNo()
+        return flask.Response(flask.json.dumps({"message": ""}), status=200, mimetype='application/json')
 
 
 if __name__ == "__main__":
